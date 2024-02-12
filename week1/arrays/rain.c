6a159cf771f974885ad89cbd7eb8ffa34451622b
CStalks <swiafe.dev@gmail.com>
2024-02-11 20:34:10 -0500

    Complete function rain.c

--- week1/arrays/rain.c
+++ week1/arrays/rain.c
@@ -18,9 +18,9 @@ int main(void){
 
   for(year = 0, total = 0; year < YEARS; year++){
     for (month = 0, subtot = 0; month < MONTHS; month++)
-      subtot += rain[year][month]; (rain + month)
-  printf("%5d %15.1f\n", 2010 + year, subtot);
-  total += subtot;
+      subtot += *(*(rain+year)+month);
+    printf("%5d %15.1f\n", 2010 + year, subtot);
+    total += subtot;
 
   printf("\nThe yearly average is %.1f inches.\n\n", total/YEARS);
   printf("MONTHLY AVERAGES:\n\n");
@@ -29,9 +29,10 @@ int main(void){
 
   for (month = 0; month < MONTHS; month++){ 
     for (year = 0, subtot =0; year < YEARS; year++)
-      subtot += rain[year][month];
+      subtot += *(*(rain+year)+month);
     printf("%4.1f ", subtot/YEARS);
-  }printf("\n");
+  }
+  printf("\n");
 
  return 0;
 }
